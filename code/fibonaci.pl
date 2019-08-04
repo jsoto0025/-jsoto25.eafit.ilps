29088e7bd2a87aa659c9b97ca5e70f4a035800fd
jsoto25@hotmail.com <jsoto25@hotmail.com>
2019-08-04 07:44:01 -0500

    Programa que haya el valor de la serie fibonaci de un nemero dado.

---
+++ code/fibonaci.pl
@@ -0,0 +1,14 @@
+%Autor:         jsoto25@hotmail.com
+%Descripción:   Programa en GNU Prolog que hayar la serie fibonaci entre 2 numeros dados.
+%               Forma de uso: fibo(X,8).
+%Fecha:         20190803
+
+fibo(0,1).
+   fibo(1,1).
+   fibo(N,X):- 
+     N > 1, 
+     N1 is N-1, 
+     N2 is N-2, 
+     fibo(N1,F1), 
+     fibo(N2,F2), 
+     X is F1+F2.
